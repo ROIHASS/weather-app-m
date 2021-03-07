911dfe2decbc53f2e33cbb4521be1e745c41604d
ROIHASS <hassonroi@gmail.com>
2021-03-07 11:00:06 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,7 @@ import { trigger, style, animate, transition } from '@angular/animations';
       transition(':enter', [
         style({  opacity: 0 }),
         animate('500ms', style({  opacity: 1 }))
-      ]),
-      transition(':leave', [
-        style({  }),
-        animate('500ms', style({  opacity: 0 }))
-      ])
-    ]
+      ])]
     )
   ],
 })
